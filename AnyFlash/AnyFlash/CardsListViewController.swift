dacfc22958fd9d0c62b3dae9fdc0be293b484728
isaacJChen <isaacjc@uw.edu>
2018-03-10 20:12:17 -0800

    able show cards in each category

--- AnyFlash/AnyFlash/CardsListViewController.swift
+++ AnyFlash/AnyFlash/CardsListViewController.swift
@@ -7,13 +7,36 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
-class CardsListViewController: UIViewController {
+class CardsListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flashCardData == nil ? 0 : flashCardData.count-1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cardCell", for: indexPath)
+        
+        let keys = flashCardData.allKeys
+        let label = keys[indexPath.row] as? String
+        cell.textLabel?.text = label == "cardPlaceHolderKey" ? keys[keys.count-1] as? String : label
+        cell.detailTextLabel?.text = flashCardData.object(forKey: cell.textLabel?.text) as! String
+        tableView.tableFooterView = UIView()
+        
+        return cell
+    }
+    
     
     var uid = ""
     var category = ""
+    var flashCardData: NSDictionary!
+    @IBOutlet weak var table: UITableView!
+    
 
     @IBOutlet weak var courseNameLabel: UILabel!
+    
+    var ref: DatabaseReference! = Database.database().reference()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +47,19 @@ class CardsListViewController: UIViewController {
         print(self.category)
         
         courseNameLabel.text = self.category
+        
+        table.dataSource = self
+        table.delegate = self
+        
+        ref = Database.database().reference()
+        // Do any additional setup after loading the view.
+        ref.child("users").child(self.uid).child(category).observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            self.flashCardData = (snapshot.value as? NSDictionary)!
+            self.table.reloadData()
+        }) { (error) in
+            print(error.localizedDescription)
+        }
     }
 
     override func didReceiveMemoryWarning() {
