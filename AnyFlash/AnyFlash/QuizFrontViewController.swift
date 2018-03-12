d3bb30d1109037e9af43820f4b053508c03e6934
Qiaosi Wang <wqs@uw.edu>
2018-03-11 18:07:57 -0700

    flip functioning

--- AnyFlash/AnyFlash/QuizFrontViewController.swift
+++ AnyFlash/AnyFlash/QuizFrontViewController.swift
@@ -14,30 +14,18 @@ class QuizFrontViewController: UIViewController {
     var category = ""
     var flashCardData: NSDictionary!
     var currentIndex = 0
+    var isFlipped = false
+    var keys : [Any] = []
+    var values : [Any] = []
     @IBOutlet weak var numOutaNum: UILabel!
     
     @IBOutlet weak var cardFrontLabel: UILabel!
     
     override func viewDidLoad() {
+        keys = flashCardData.allKeys
+        values = flashCardData.allValues
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        let keys = flashCardData.allKeys
-        var text = ""
-        if self.currentIndex == keys.count-1 {
-            self.currentIndex = 0
-            text = (keys[currentIndex] as? String)!
-        } else {
-            text = (keys[currentIndex] as? String)!
-        }
-        
-        if text != "cardPlaceHolderKey" {
-            self.cardFrontLabel.text = keys[currentIndex] as? String
-        } else {
-            self.cardFrontLabel.text = keys.last as? String
-        }
-        
-        numOutaNum.text = "\(self.currentIndex+1)/\(self.flashCardData.count-1)"
+        cardFrontLabel.text = keys[currentIndex] as? String
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,38 +33,65 @@ class QuizFrontViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func btnFlip(_ sender: Any) {
+        var frontText = ""
+        var backText = ""
+        if isFlipped{
+            isFlipped = false
+            if self.currentIndex == keys.count-1 {
+                self.currentIndex = 0
+                frontText = (keys[currentIndex] as? String)!
+            } else {
+                frontText = (keys[currentIndex] as? String)!
+            }
+
+            if frontText != "cardPlaceHolderKey" {
+                self.cardFrontLabel.text = keys[currentIndex] as? String
+            } else {
+                self.cardFrontLabel.text = keys.last as? String
+            }
+            numOutaNum.text = "\(self.currentIndex+1)/\(self.flashCardData.count-1)"
+
+            UIView.transition(with: cardFrontLabel, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+        } else {
+            isFlipped = true
+            if self.currentIndex == values.count-1 {
+                self.currentIndex = 0
+                backText = (values[currentIndex] as? String)!
+            } else {
+                backText = (values[currentIndex] as? String)!
+            }
+
+            if backText != "cardValue" {
+                self.cardFrontLabel.text = values[currentIndex] as? String
+            } else {
+                self.cardFrontLabel.text = values.last as? String
+            }
+            numOutaNum.text = "\(self.currentIndex+1)/\(self.flashCardData.count-1)"
+
+            UIView.transition(with: cardFrontLabel, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+        }
+    }
+    
+    
+    
     @IBAction func nextCardPressed(_ sender: Any) {
-        let keys = flashCardData.allKeys
         self.currentIndex+=1
         var text = ""
+        isFlipped = false
         if self.currentIndex == keys.count-1 {
             self.currentIndex = 0
             text = (keys[currentIndex] as? String)!
         } else {
             text = (keys[currentIndex] as? String)!
         }
-        
+
         if text != "cardPlaceHolderKey" {
             self.cardFrontLabel.text = keys[currentIndex] as? String
         } else {
             self.cardFrontLabel.text = keys.last as? String
         }
         numOutaNum.text = "\(self.currentIndex+1)/\(self.flashCardData.count-1)"
-    }
-    
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "backFromQuizFront" {
-            let destination = segue.destination as! CardsListViewController
-            destination.uid = self.uid
-            destination.category = self.category
-        } else if segue.identifier == "flip" {
-            let destination = segue.destination as! QuizBackViewController
-            destination.uid = self.uid
-            destination.category = self.category
-            destination.currentIndex = self.currentIndex
-            destination.flashCardData = self.flashCardData
-        }
     }
 
 
